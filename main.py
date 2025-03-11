9ce8e4037c708c60b056a4082e9ba6fdbd8d2f95
Prakash karna <prakashkarna.kr@gmail.com>
2025-03-12 00:06:46 +0545

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Importing the required libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# This is a single line comment that provides title for this application
+# Title of the web app
+st.title('Getting Started with Machine Learning')
+
